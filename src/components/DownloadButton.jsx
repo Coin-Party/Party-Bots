c0ad7bb54228d1489d45d1ae41538988b4d85178
Paul Gradie <paul.e.gradie@gmail.com>
2024-11-21 11:28:21 +1100

    update the download button

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@/components/Button';
 
+const extractWindowsVersion = (url) => {
+    const match = url.match(/PartyBots-Windows-([\d.]+)\.zip/);
+    return match ? match[1] : null;
+};
+const extractUnixVersion = (url) => {
+    const match = url.match(/PartyBots-Unix-([\d.]+)\.zip/);
+    return match ? match[1] : null;
+};
+
 const DownloadSection = ({ windowsVersionLink, unixVersionLink, loading }) => {
+
+    let windowsVersion;
+    let unixVersion;
+    if (!loading) {
+        windowsVersion = extractWindowsVersion(windowsVersionLink);
+        unixVersion = extractUnixVersion(unixVersionLink);
+    } else {
+
+        windowsVersion = "";
+        unixVersion = "";
+    }
+
+
     return (
         <div className="mt-8 flex flex-col gap-8 md:justify-center lg:justify-center items-center">
             {/* Download text styled to be large, bold, and white */}
             <div className="text-4xl font-bold text-white">
                 Download
             </div>
-
             {/* Button section */}
             <div className="flex gap-4">
                 <Button
@@ -16,14 +37,14 @@ const DownloadSection = ({ windowsVersionLink, unixVersionLink, loading }) => {
                     disabled={loading || !windowsVersionLink}
                     className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 disabled:opacity-50"
                 >
-                    {loading ? 'Loading...' : 'Windows'}
+                    {loading ? 'Loading...' : `Windows ${windowsVersion || ''}`}
                 </Button>
                 <Button
                     href={unixVersionLink}
                     disabled={loading || !unixVersionLink}
                     className="bg-green-600 text-white px-6 py-3 rounded-lg hover:bg-green-700 disabled:opacity-50"
                 >
-                    {loading ? 'Loading...' : 'Unix'}
+                    {loading ? 'Loading...' : `Unix  ${unixVersion || ''}`}
                 </Button>
             </div>
         </div>
